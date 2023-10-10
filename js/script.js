90f17543e84641c9c85deac5212bdf6a9bafbb8f
alexraduca01 <alex.raduca01@icloud.com>
2023-10-10 15:53:55 +0200

    finished generateButton

--- js/script.js
+++ js/script.js
@@ -13,25 +13,35 @@ const resetButton = document.querySelector('.btn-danger');
 
 generateButton.addEventListener('click', function(){
     const nameSurname = document.getElementById('namesurname').value;
-    const distance = document.getElementById('distance').value;
+    const distance = parseInt(document.getElementById('distance').value);
     const age = document.getElementById('age').value;
     const basePrice = (0.21 * distance);
+    const ticket = document.getElementById('ticket');
     let finalPrice;
-    
-    console.log(nameSurname);
-    console.log(distance);
-    console.log(age);
 
-    if (age === 'select' || nameSurname === ''){
-        alert('devi inserire dei valori!')
+    if (age === 'select' || nameSurname === '' || isNaN(distance)){
+        alert('devi inserire dei valori corretti!')
     } else {
         if(age === 'minor'){
             finalPrice = (basePrice - (basePrice * 20 / 100)).toFixed(2);
+            ticket.classList.toggle("d-none");
+            document.querySelector('.rate').innerHTML = 'Scontato del 20%';
+            document.querySelector('.price').innerHTML = finalPrice + " €";
         } else if (age === 'over65'){
             finalPrice = (basePrice - (basePrice * 40 / 100)).toFixed(2);
+            ticket.classList.toggle("d-none");
+            document.querySelector('.rate').innerHTML = 'Scontato del 40%';
+            document.querySelector('.price').innerHTML = finalPrice + " €";
         } else {
             finalPrice = basePrice.toFixed(2);
+            ticket.classList.toggle("d-none");
+            document.querySelector('.rate').innerHTML = 'Biglietto standard';
+            document.querySelector('.price').innerHTML = finalPrice + " €";
         }
     }
-        
+
+    document.querySelector('.row > div > p').innerHTML = nameSurname;
+    document.querySelector('.carriage').innerHTML = (Math.floor(Math.random() * 10) + 1);
+    document.querySelector('.cp').innerHTML = (Math.floor(Math.random() * 10000) + 1);
+         
 })
